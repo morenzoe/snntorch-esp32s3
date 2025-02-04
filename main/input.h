696eb96a0eca19a315227ce4c62cf206834beb88
Eraraya Morenzo Muten <morenzomuten@gmail.com>
2025-02-04 19:27:42 +0700

    Added input from indy_20160622_01

--- main/input.h
+++ main/input.h
@@ -1,5 +1,4 @@
 #define INPUT_NEURONS_NUM 96
-// #define T 137545 // input array size
-#define T 11 // input array size
+#define T 137545 // input array size
 
 extern const unsigned char input_z[T][1][INPUT_NEURONS_NUM / 8];
